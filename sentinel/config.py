0f5f790c28e65fee9373809857adacf74a2b322a
Tikhon Roshchupkin <tikhon@nebius.com>
2026-05-27 16:46:21 +0100

    change default max parallel audits

--- sentinel/config.py
+++ sentinel/config.py
@@ -19,7 +19,7 @@ NEBIUS_MODELS = {
 MODEL = NEBIUS_MODELS.get(os.environ.get("NEBIUS_MODEL", "v4"), NEBIUS_MODELS["v4"])
 MODEL_MAX_TOKENS = 16_000
 REASONING_EFFORT = os.environ.get("REASONING_EFFORT", "off")  # off, high, max
-MAX_AUDIT_WORKERS = int(os.environ.get("MAX_AUDIT_WORKERS", "50"))
+MAX_AUDIT_WORKERS = int(os.environ.get("MAX_AUDIT_WORKERS", "200"))
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
 OPENAI_MODEL = "gpt-5.5"
